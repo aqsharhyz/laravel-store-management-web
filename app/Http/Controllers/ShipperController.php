d69560b648584ce0ee15072d684d992e1c8207e7
Aqsha Rheyznandya Malik <aqsharheyznandyamalik@mail.ugm.ac.id>
2024-04-30 22:50:54 +0300

    Update ShipperController to have basic functionality and use resource requests and return JSON responses

--- app/Http/Controllers/ShipperController.php
+++ app/Http/Controllers/ShipperController.php
@@ -2,7 +2,12 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\CreateShipperRequest;
+use App\Http\Requests\UpdateShipperRequest;
+use App\Http\Resources\ShipperCollection;
+use App\Http\Resources\ShipperResource;
 use App\Models\Shipper;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ShipperController extends Controller
@@ -10,9 +15,10 @@ class ShipperController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(): ShipperCollection
     {
-        //
+        $shippers = Shipper::all();
+        return new ShipperCollection($shippers);
     }
 
     /**
@@ -26,17 +32,22 @@ class ShipperController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(CreateShipperRequest $request): JsonResponse
     {
-        //
+        $validated = $request->validated();
+
+        $shipper = new Shipper($validated);
+        $shipper->save();
+
+        return (new ShipperResource($shipper))->response()->setStatusCode(201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(Shipper $shipper)
+    public function show(Shipper $shipper): ShipperResource
     {
-        //
+        return new ShipperResource($shipper);
     }
 
     /**
@@ -50,16 +61,22 @@ class ShipperController extends Controller
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Shipper $shipper)
+    public function update(UpdateShipperRequest $request, Shipper $shipper): ShipperResource
     {
-        //
+        $validated = $request->validated();
+
+        $shipper->fill($validated);
+        $shipper->save();
+
+        return new ShipperResource($shipper);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Shipper $shipper)
+    public function destroy(Shipper $shipper): JsonResponse
     {
-        //
+        $shipper->delete();
+        return response()->json(['data' => true]);
     }
 }
---
+++ app/Http/Requests/UpdateShipperRequest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateShipperRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'name' => ['max:100', 'unique:shippers'],
+            'phone' => ['max:25'],
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response([
+            "errors" => $validator->getMessageBag()
+        ], 400));
+    }
+}
